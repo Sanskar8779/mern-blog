a2499776eed93d9bb19b8d54684b68b26b5f7495
Sanskar <sanskar.shubham28@gmail.com>
2024-01-22 01:29:14 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,34 @@
-// import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
 
 export default function App() {
   return (
-    <h1 className="text-3xl text-red-500">app</h1>
-  )
+    <BrowserRouter>
+      {/* <ScrollToTop /> */}
+      {/* <Header /> */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='/sign-up' element={<SignUp />} />
+        {/* <Route path='/search' element={<Search />} /> */}
+        {/* <Route element={<PrivateRoute />}> */}
+          <Route path='/dashboard' element={<Dashboard />} />
+        {/* </Route> */}
+        {/* <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:postId' element={<UpdatePost />} />
+        </Route> */}
+
+        <Route path='/projects' element={<Projects />} />
+        {/* <Route path='/post/:postSlug' element={<PostPage />} /> */}
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
+  );
 }
