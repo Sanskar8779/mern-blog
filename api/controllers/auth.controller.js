4ff0bcf95bdfecf2cf1e93723fe743a653df1e7c
Sanskar <sanskar.shubham28@gmail.com>
2024-04-24 19:49:50 +0530

    create signup api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,37 +1,38 @@
-// import User from '../models/user.model.js';
-// import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
 // import { errorHandler } from '../utils/error.js';
 // import jwt from 'jsonwebtoken';
 
-// export const signup = async (req, res, next) => {
-//   const { username, email, password } = req.body;
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
 
-//   if (
-//     !username ||
-//     !email ||
-//     !password ||
-//     username === '' ||
-//     email === '' ||
-//     password === ''
-//   ) {
-//     next(errorHandler(400, 'All fields are required'));
-//   }
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    // next(errorHandler(400, 'All fields are required'));
+    return res.status(400).json({message: 'All Fields are required'});
+  }
 
-//   const hashedPassword = bcryptjs.hashSync(password, 10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-//   const newUser = new User({
-//     username,
-//     email,
-//     password: hashedPassword,
-//   });
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
 
-//   try {
-//     await newUser.save();
-//     res.json('Signup successful');
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+  try {
+    await newUser.save();
+    res.json('Signup successful');
+  } catch (error) {
+    next(error);
+  }
+};
 
 // export const signin = async (req, res, next) => {
 //   const { email, password } = req.body;
@@ -113,3 +114,7 @@
 //     next(error);
 //   }
 // };
+
+// export const signup = async (req, res) => {
+//     console.log(req.body);
+// }
