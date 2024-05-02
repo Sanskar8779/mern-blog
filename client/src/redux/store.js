fee01f98b4c57ab67560950d087a9849ce990b7d
Sanskar <sanskar.shubham28@gmail.com>
2024-05-02 14:48:26 +0530

    add redux toolkit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,5 @@
-// import { configureStore, combineReducers } from '@reduxjs/toolkit';
-// import userReducer from './user/userSlice';
+import { configureStore, /*combineReducers*/ } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
 // import themeReducer from './theme/themeSlice';
 // import { persistReducer, persistStore } from 'redux-persist';
 // import storage from 'redux-persist/lib/storage';
@@ -17,10 +17,13 @@
 
 // const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// export const store = configureStore({
+export const store = configureStore({
 //   reducer: persistedReducer,
 //   middleware: (getDefaultMiddleware) =>
 //     getDefaultMiddleware({ serializableCheck: false }),
-// });
+    reducer: {
+        user: userReducer,
+    },
+});
 
 // export const persistor = persistStore(store);
