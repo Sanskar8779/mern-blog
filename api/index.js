af742087b1458328021ea619a85df7a04d02dcea
Sanskar <sanskar.shubham28@gmail.com>
2024-07-23 20:48:22 +0530

    add update user api route

--- api/index.js
+++ api/index.js
@@ -3,10 +3,10 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 
 // import postRoutes from './routes/post.route.js';
 // import commentRoutes from './routes/comment.route.js';
-// import cookieParser from 'cookie-parser';
 // import path from 'path';
 
 
@@ -25,7 +25,7 @@ mongoose.connect(process.env.MONGO).then(
 const app = express();
 
 app.use(express.json());
-// app.use(cookieParser());
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000!");
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -5,14 +5,14 @@ import {
 //   getUsers,
 //   signout,
   test,
-//   updateUser,
+  updateUser,
 } from '../controllers/user.controller.js';
-// import { verifyToken } from '../utils/verifyUser.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/test', test);
-// router.put('/update/:userId', verifyToken, updateUser);
+router.put('/update/:userId' , verifyToken, updateUser);
 // router.delete('/delete/:userId', verifyToken, deleteUser);
 // router.post('/signout', signout);
 // router.get('/getusers', verifyToken, getUsers);
