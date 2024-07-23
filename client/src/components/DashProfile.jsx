fbcee14775fbaac54e7ee7cb306b2fd9b0439ed8
Sanskar <sanskar.shubham28@gmail.com>
2024-07-23 16:07:30 +0530

    Complete profile page UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,6 +1,6 @@
-// import { Alert, Button, Modal, ModalBody, TextInput } from 'flowbite-react';
-// import { useEffect, useRef, useState } from 'react';
-// import { useSelector } from 'react-redux';
+import { Alert, Button, Modal, ModalBody, TextInput } from 'flowbite-react';
+import { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
 // import {
 //   getDownloadURL,
 //   getStorage,
@@ -19,9 +19,9 @@
 //   deleteUserFailure,
 //   signoutSuccess,
 // } from '../redux/user/userSlice';
-// import { useDispatch } from 'react-redux';
-// import { HiOutlineExclamationCircle } from 'react-icons/hi';
-// import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 
 // export default function DashProfile() {
 //   const { currentUser, error, loading } = useSelector((state) => state.user);
@@ -304,10 +304,55 @@
 //   );
 // }
 
-
-
 export default function DashProfile() {
+    const {currentUser} = useSelector(state => state.user)
     return (
-        <div>DashProfile</div>
+        <div className='max-w-lg mx-auto p-3 w-full'>
+            <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
+            <form className='flex flex-col gap-4'>
+                <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+                    <img src={currentUser.profilePicture} alt="user" 
+                    className='rounded-full w-full h-full object-cover border-8 border-[lightgray]'
+                    />
+                </div>
+                <TextInput
+                    type='text'
+                    id='username'
+                    placeholder='username'
+                    defaultValue={currentUser.username}
+                    // onChange={handleChange}
+                />
+                <TextInput
+                    type='email'
+                    id='email'
+                    placeholder='email'
+                    defaultValue={currentUser.email}
+                    // onChange={handleChange}
+                />
+                <TextInput
+                    type='password'
+                    id='password'
+                    placeholder='password'
+                    // onChange={handleChange}
+                />
+                <Button
+                    type='submit'
+                    gradientDuoTone='purpleToBlue'
+                    outline
+                    // disabled={loading || imageFileUploading}
+                >
+                    {/* {loading ? 'Loading...' : 'Update'} */}
+                    Update
+                </Button>
+            </form>
+            <div className='text-red-500 flex justify-between mt-5'>
+                <span /*onClick={() => setShowModal(true)} */className='cursor-pointer'>
+                    Delete Account
+                </span>
+                <span /*onClick={handleSignout} */className='cursor-pointer'>
+                    Sign Out
+                </span>
+            </div>
+        </div>
     )
 }
