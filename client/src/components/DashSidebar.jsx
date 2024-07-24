de45aaac25635fcae51dc3aeb3df03ad2522deb0
Sanskar <sanskar.shubham28@gmail.com>
2024-07-24 17:34:36 +0530

    add sign functionlaity

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,22 +1,13 @@
 import { Sidebar } from 'flowbite-react';
-import {
-  HiUser,
-  HiArrowSmRight,
-  HiDocumentText,
-  HiOutlineUserGroup,
-  HiAnnotation,
-  HiChartPie,
-} from 'react-icons/hi';
+import { HiUser, HiArrowSmRight } from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-// import { signoutSuccess } from '../redux/user/userSlice';
+import { signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
 
 export default function DashSidebar() {
   const location = useLocation();
-//   const dispatch = useDispatch();
-//   const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const [tab, setTab] = useState('');
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -25,41 +16,29 @@ export default function DashSidebar() {
       setTab(tabFromUrl);
     }
   }, [location.search]);
-//   const handleSignout = async () => {
-//     try {
-//       const res = await fetch('/api/user/signout', {
-//         method: 'POST',
-//       });
-//       const data = await res.json();
-//       if (!res.ok) {
-//         console.log(data.message);
-//       } else {
-//         dispatch(signoutSuccess());
-//       }
-//     } catch (error) {
-//       console.log(error.message);
-//     }
-//   };
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
-        <Sidebar.ItemGroup className='flex flex-col gap-1'>
-          {/* {currentUser && currentUser.isAdmin && (
-            <Link to='/dashboard?tab=dash'>
-              <Sidebar.Item
-                active={tab === 'dash' || !tab}
-                icon={HiChartPie}
-                as='div'
-              >
-                Dashboard
-              </Sidebar.Item>
-            </Link>
-          )} */}
+        <Sidebar.ItemGroup>
           <Link to='/dashboard?tab=profile'>
             <Sidebar.Item
               active={tab === 'profile'}
               icon={HiUser}
-            //   label={currentUser.isAdmin ? 'Admin' : 'User'}
               label={'User'}
               labelColor='dark'
               as='div'
@@ -67,43 +46,10 @@ export default function DashSidebar() {
               Profile
             </Sidebar.Item>
           </Link>
-          {/* {currentUser.isAdmin && (
-            <Link to='/dashboard?tab=posts'>
-              <Sidebar.Item
-                active={tab === 'posts'}
-                icon={HiDocumentText}
-                as='div'
-              >
-                Posts
-              </Sidebar.Item>
-            </Link>
-          )} */}
-          {/* {currentUser.isAdmin && (
-            <>
-              <Link to='/dashboard?tab=users'>
-                <Sidebar.Item
-                  active={tab === 'users'}
-                  icon={HiOutlineUserGroup}
-                  as='div'
-                >
-                  Users
-                </Sidebar.Item>
-              </Link>
-              <Link to='/dashboard?tab=comments'>
-                <Sidebar.Item
-                  active={tab === 'comments'}
-                  icon={HiAnnotation}
-                  as='div'
-                >
-                  Comments
-                </Sidebar.Item>
-              </Link>
-            </>
-          )} */}
           <Sidebar.Item
             icon={HiArrowSmRight}
             className='cursor-pointer'
-            // onClick={handleSignout}
+            onClick={handleSignout}
           >
             Sign Out
           </Sidebar.Item>
@@ -112,7 +58,3 @@ export default function DashSidebar() {
     </Sidebar>
   );
 }
-
-
-
-
